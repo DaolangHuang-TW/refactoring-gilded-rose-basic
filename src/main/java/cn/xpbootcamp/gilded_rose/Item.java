4b67a994af0bf9ba7ec419418c646185aa80e6d9
daolang.huang <daolang.huang@thoughtworks.com>
2020-05-20 20:49:42 +0800

    hidden the field in Item, provide get/set method.

--- src/main/java/cn/xpbootcamp/gilded_rose/Item.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/Item.java
@@ -2,17 +2,38 @@ package cn.xpbootcamp.gilded_rose;
 
 public class Item {
 
-    public String name;
+    private String name;
 
-    public int sell_in;
+    private int sell_in;
 
-    public int quality;
+    private int quality;
 
     public Item(String name, int sell_in, int quality) {
         this.name = name;
         this.sell_in = sell_in;
         this.quality = quality;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSellIn() {
+        return sell_in;
+    }
+
+    public void setSellIn(int sellIn) {
+        this.sell_in = sellIn;
+    }
+
+    public void setQuality(int quality) {
+        this.quality = quality;
+    }
+
+    public int getQuality() {
+        return quality;
+    }
+
 
     @Override
     public String toString() {
